db8d7cf8b87dc14882f35535d5968d962a672c63
Melissa2016 <32329679+Melissa2016@users.noreply.github.com>
2018-02-27 15:43:34 +0100

    hi

--- dump/status.js
+++ dump/status.js
@@ -1,7 +1,7 @@
 
 
 'use strict';
-
+/*
 var weatherData = [
     {
         time: "09:00",
@@ -50,7 +50,7 @@ for(var i=0;i<weatherData.length;i++){
     
     tbody.appendChild(tr);
 }
-
+*/
 //console.log( weatherData[1]);
 
 
@@ -81,7 +81,7 @@ var tågdata = [
             },
    
 ]
-
+/*   */
 
 var trainBody = document.getElementById("tåg");   
 function createTrainTable(){
